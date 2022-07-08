9bb81534ccb539f40cfd65742a54f1e4a220a032
Manop Aemsiri <manop.aemsiri@gmail.com>
2022-07-09 02:29:28 +0700

    Update Structure

--- 01-home.php
+++ 01-home.php
@@ -6,9 +6,106 @@
 </head>
 <body class="loading">
   <?php include_once('include/topnav.php'); ?>
-  
-  <img class="img" src="public/assets/app/images/main.jpg" alt="Page" />
 
+
+  <section class="section-01" style="background-image: url(public/assets/app/images/bg/01.jpg);">
+    
+  </section>
+
+  <section class="section-02 section-padding">
+    <div class="container">
+      <div class="text-center">
+        <h3 class="fw-700">ข่าวสาร/ประชาสัมพันธ์</h3>
+        <p>ติดตามข่าวสารต่างๆ พร้อมลิงก์ที่น่าสนใจของ กกต.ได้ที่นี่</p>
+      </div>
+      <div class="btns jc-center mt-3">
+          <a href="#" class="btn btn-action btn-p">
+            บทความทั้งหมด
+          </a>
+          <a href="#" class="btn btn-action btn-s">
+            บทความทั้งหมด
+          </a>
+          <a href="#" class="btn btn-action btn-inactive">
+            บทความทั้งหมด
+          </a>
+          <a href="#" class="btn btn-action btn-inactive">
+            บทความทั้งหมด
+          </a>
+      </div>
+      <div class="grids mt-5">
+        <div class="grid lg-60">
+          <div class="">
+            <img src="public/assets/app/images/bg/demo02.jpg" class="img"/>
+          </div>
+        </div>
+        <div class="grid lg-40">
+          <div class="">
+            <img src="public/assets/app/images/bg/demo03.jpg" class="img"/>
+          </div>
+          <div class="">
+            <img src="public/assets/app/images/bg/demo04.jpg" class="img mt-2"/>
+          </div>
+        </div>
+      </div>
+      <div class="btns jc-center mt-3">
+          <a href="#" class="btn btn-action btn-p">
+            ข่าวประชาสัมพันธ์ทั้งหมด
+          </a>
+      </div>
+    </div>
+  </section>
+
+  <section class="section-03 section-padding bg-p">
+    <div class="container">
+      <div class="grids">
+        <div class="grid lg-50">
+
+        </div>
+        <div class="grid lg-50">
+          <div>
+            <h2 class="color-white fw-700">กกต. พร้อมให้บริการข้อมูลการเลือกตั้งแก่ภาคประชาชนทุกภาคส่วนด้วยหลากหลายแอพพลิเคชั่น</h2>
+          </div>
+          <div class="grids">
+            <div class="grid lg-1-3 md-1-3">
+              <div class="pos-relative">
+                <img src="public/assets/app/images/bg/demo05.jpg" class="img bradius-round"/>
+                <div class="pos-absolute text-wrapper">
+                  <h6>แอปพลิเคชั่นฉลาดเลือก</h6>
+                </div>
+              </div>
+            </div>
+            <div class="grid lg-1-3 md-1-3">
+              <div class="pos-relative">
+                <img src="public/assets/app/images/bg/demo05.jpg" class="img bradius-round"/>
+                <div class="pos-absolute text-wrapper">
+                  <h6>แอปพลิเคชั่นฉลาดเลือก</h6>
+                </div>
+              </div>
+            </div>
+            <div class="grid lg-1-3 md-1-3">
+              <div class="pos-relative">
+                <img src="public/assets/app/images/bg/demo05.jpg" class="img bradius-round"/>
+                <div class="pos-absolute text-wrapper">
+                  <h6>แอปพลิเคชั่นฉลาดเลือก</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+ 
+
+
+
+
+
+
+  
+  <!-- <img class="img" src="public/assets/app/images/main.jpg" alt="Page" />  -->
+<!-- 
   <section class="section-padding">
     <div class="container">
       <div class="btns jc-center">
@@ -23,9 +120,9 @@
         </a>
       </div>
     </div>
-  </section>
+  </section> -->
     
   <?php // include_once('include/footer.php'); ?>
-  <?php include_once('include/script.php'); ?>
+  <?php  include_once('include/script.php'); ?>
 </body>
 </html>
