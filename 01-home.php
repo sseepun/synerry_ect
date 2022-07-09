205953143794460d50186818a4cabe07d0dec777
Manop Aemsiri <manop.aemsiri@gmail.com>
2022-07-09 17:43:26 +0700

    Update structure

--- 01-home.php
+++ 01-home.php
@@ -33,21 +33,39 @@
           </a>
       </div>
       <div class="grids mt-5">
-        <div class="grid lg-60">
-          <div class="">
-            <img src="public/assets/app/images/bg/demo02.jpg" class="img"/>
+        <div class="grid lg-70 md-60 sm-50 xs-100">
+          <div class="pos-relative">
+            <img src="public/assets/app/images/bg/demo02.jpg" class="img bradius-round" style="height:525px;"/>
+            <div class="pos-absolute text-wrapper ss-text-wrapper sm color-white">
+              <a href="#" class="d-inline-block bg-p bradius-round color-white pt-1 pr-3 pb-1 pl-3">เรื่องเด่น กกต.</a>
+              <p class="mt-3">การอบรมหลักสูตรการปฏิบัติงานระดับการชำนาญพิเศษ</p>
+              <p>กรรมการเลือกตั้ง มอบนโยบายในการปฏิบัติงาน แนวทางแก้ไขปัญหาในการปฏิบัติงานให้แก่พนักงานในการอบรมหลักสูตร</p>
+              <p class="mt-3">08 มิถุนายน 2565 / By <span><a href="#">PR ECT</a></span></p>
+            </div>
           </div>
         </div>
-        <div class="grid lg-40">
-          <div class="">
-            <img src="public/assets/app/images/bg/demo03.jpg" class="img"/>
+        <div class="grid lg-30 md-40 sm-50 xs-100">
+          <div class="pos-relative">
+          <img src="public/assets/app/images/bg/demo03.jpg" class="img bradius-round" style="height:252px;"/>
+            <div class="pos-absolute text-wrapper ss-text-wrapper sm color-white">
+              <a href="#" class="d-inline-block bg-p bradius-round color-white pt-1 pr-3 pb-1 pl-3">เรื่องเด่น กกต.</a>
+              <p class="mt-3">การอบรมหลักสูตรการปฏิบัติงานระดับการชำนาญพิเศษ</p>
+              <p class="mt-3">08 มิถุนายน 2565 / By <span><a href="#">PR ECT</a></span></p>
+            </div>
           </div>
-          <div class="">
-            <img src="public/assets/app/images/bg/demo04.jpg" class="img mt-2"/>
+          <div class="grid lg-30 md-40 sm-50 xs-100">
+           <div class="pos-relative pt-5">
+            <img src="public/assets/app/images/bg/demo03.jpg" class="img bradius-round" style="height:252px;"/>
+             <div class="pos-absolute text-wrapper ss-text-wrapper sm color-white">
+               <a href="#" class="d-inline-block bg-p bradius-round color-white pt-1 pr-3 pb-1 pl-3">เรื่องเด่น กกต.</a>
+               <p class="mt-3">การอบรมหลักสูตรการปฏิบัติงานระดับการชำนาญพิเศษ</p>
+               <p class="mt-3">08 มิถุนายน 2565 / By <span><a href="#">PR ECT</a></span></p>
+             </div>
+           </div>
           </div>
         </div>
       </div>
-      <div class="btns jc-center mt-3">
+      <div class="btns jc-center mt-5">
           <a href="#" class="btn btn-action btn-p">
             ข่าวประชาสัมพันธ์ทั้งหมด
           </a>
@@ -59,7 +77,7 @@
     <div class="container">
       <div class="grids">
         <div class="grid lg-50">
-
+           <!-- Insert Image -->
         </div>
         <div class="grid lg-50">
           <div>
@@ -88,6 +106,54 @@
                 <div class="pos-absolute text-wrapper">
                   <h6>แอปพลิเคชั่นฉลาดเลือก</h6>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section-04 section-padding">
+    <div class="container">
+      <h3 class="fw-700">องค์ความรู้การเลือกตั้ง</h3>
+      <div class="grids">
+        <div class="grid lg-50">
+          <div class="grids">
+            <div class="grid lg-50">
+              <div class="ss-text-wrapper">
+                <h5>เลือกตั้งกรุงเทพมหานครและเมืองพัทยา</h5>
+                <p>
+                  วันอาทิตย์ที่ 22 พฤษภาคม 2565 เวลา 08.00 - 17.00 น. เลือกตั้งสมาชิกสภาท้องถิ่นและผู้
+                  บริหารท้องถิ่นของกรุงเทพมหานครและเมือง
+                </p>
+              </div>
+            </div>
+            <div class="grid lg-50">
+              <div class="ss-text-wrapper">
+                <h5>เลือกตั้งกรุงเทพมหานครและเมืองพัทยา</h5>
+                <p>
+                  วันอาทิตย์ที่ 22 พฤษภาคม 2565 เวลา 08.00 - 17.00 น. เลือกตั้งสมาชิกสภาท้องถิ่นและผู้
+                  บริหารท้องถิ่นของกรุงเทพมหานครและเมือง
+                </p>
+              </div>
+            </div>
+            <div class="grid lg-50">
+              <div class="ss-text-wrapper">
+                <h5>เลือกตั้งกรุงเทพมหานครและเมืองพัทยา</h5>
+                <p>
+                  วันอาทิตย์ที่ 22 พฤษภาคม 2565 เวลา 08.00 - 17.00 น. เลือกตั้งสมาชิกสภาท้องถิ่นและผู้
+                  บริหารท้องถิ่นของกรุงเทพมหานครและเมือง
+                </p>
+              </div>
+            </div>
+            <div class="grid lg-50">
+              <div class="ss-text-wrapper">
+                <h5>เลือกตั้งกรุงเทพมหานครและเมืองพัทยา</h5>
+                <p>
+                  วันอาทิตย์ที่ 22 พฤษภาคม 2565 เวลา 08.00 - 17.00 น. เลือกตั้งสมาชิกสภาท้องถิ่นและผู้
+                  บริหารท้องถิ่นของกรุงเทพมหานครและเมือง
+                </p>
               </div>
             </div>
           </div>
