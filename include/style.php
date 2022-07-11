5265b75dd3b1042bfe89d2cca63b59de9ca9cfcc
Manop Aemsiri <manop.aemsiri@gmail.com>
2022-07-11 15:20:11 +0700

    update style

--- include/style.php
+++ include/style.php
@@ -3,6 +3,12 @@
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@100;200;300;400;500;600;700;800;900&display=swap" />
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
 
 <link rel="stylesheet" href="public/assets/node_modules/slick-carousel/slick/slick.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" integrity="sha512-10/jx2EXwxxWqCLX/hHth/vu2KY3jCF70dCQB8TSgNjbCVAC/8vai53GfMDrO2Emgwccf2pJqxct9ehpzG+MTw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -16,5 +22,5 @@
 <link rel="stylesheet" href="public/assets/app/css/navs.css" />
 <link rel="stylesheet" href="public/assets/app/css/blocks.css" />
 
-<link rel="stylesheet" id="css-theme" href="public/assets/app/css/color-0.css" />
+<link rel="stylesheet" id="css-theme" href="public/assets/app/css/color-1.css" />
 <link rel="stylesheet" href="public/assets/app/css/ie-fix.css" />
