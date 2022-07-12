14a099a054d1c49fc2b5b1601a0082e2f208356d
Manop Aemsiri <manop.aemsiri@gmail.com>
2022-07-12 14:57:06 +0700

    ีupdate work

--- 01-home.php
+++ 01-home.php
@@ -10,8 +10,19 @@
   <?php include_once('include/topnav.php'); ?>
 
 
-  <section class="section-01" style="background-image: url(public/assets/app/images/bg/01.jpg);">
+  <section class="section-01 pos-relative" style="background-image: url(public/assets/app/images/bg/01.jpg);">
+    <div class="container">
+      <div class="grids mb-0">
+        <div class="grid lg-50">
 
+        </div>
+        <div class="grid lg-50">
+          <div class="pos-absolute" style="top:8rem">
+            <img src="public/assets/app/images/bg/19.png" class="img"/>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 
   <section class="section-02 section-padding">
@@ -20,20 +31,47 @@
         <h3 class="fw-700">ข่าวสาร/ประชาสัมพันธ์</h3>
         <p class="fw-300">ติดตามข่าวสารต่างๆ พร้อมลิงก์ที่น่าสนใจของ กกต.ได้ที่นี่</p>
       </div>
-      <ul class="categories">
-        <li><a href="#" class="fw-400">เรื่องเด่น กกต.</a></li>
-        <li><a href="#" class="active fw-400">ข่าวประชาสัมพันธ์</a></li>
-        <li><a href="#" class="fw-400">ข่าว สนง. กกต.</a></li>
-        <li><a href="#" class="fw-400">ข่าวประกวดราคา</a></li>
-        <li><a href="#" class="fw-400">วิดีโอ</a></li>
+      <ul class="categories pl-0">
+        <li>
+          <a href="#" class="fw-400">
+            <img src="public/assets/app/images/icon/02.png" alt="icon" class="va-middle mr-2" width="22" />
+            เรื่องเด่น กกต.
+          </a>
+        </li>
+        <li>
+          <a href="#" class="fw-400 active">
+            <img src="public/assets/app/images/icon/06.png" alt="icon" class="va-middle mr-2" width="22" />
+             ข่าวประชาสัมพันธ์
+          </a>
+        </li>
+        <li class="ml-3">
+          <a href="#" class="fw-400">
+            <img src="public/assets/app/images/icon/03.png" alt="icon" class="va-middle mr-2" width="22" />
+            ข่าว สนง. กกต.
+          </a>
+        </li>
+        <li class="ml-3">
+          <a href="#" class="fw-400">
+            <img src="public/assets/app/images/icon/04.png" alt="icon" class="va-middle mr-2" width="22" />
+            ข่าว ประกวดราคา
+          </a>
+        </li>
+        <li class="ml-3">
+          <a href="#" class="fw-400">
+            <img src="public/assets/app/images/icon/05.png" alt="icon" class="va-middle mr-2" width="22" />
+            วิดีโอ
+          </a>
+        </li>
       </ul>
       <div class="grids mt-5">
         <div class="grid lg-70 md-60 sm-100 xs-100">
           <div class="pos-relative">
-            <img src="public/assets/app/images/bg/03.jpg" class="img bradius-round" style="height:525px;" />
+            <a href="#">
+              <img src="public/assets/app/images/bg/03.jpg" class="img bradius-round" style="height:525px;" />
+            </a>
             <div class="pos-absolute text-wrapper lg-text-wrapper lg color-white">
               <a href="#" class="d-inline-block bg-p bradius-round color-white pt-1 pr-3 pb-1 pl-3">เรื่องเด่น กกต.</a>
-              <h6 class="mt-3 fw-500">การอบรมหลักสูตรการปฏิบัติงานระดับการชำนาญพิเศษ</h6>
+              <h6 class="mt-3 fw-500"><a href="#">การอบรมหลักสูตรการปฏิบัติงานระดับการชำนาญพิเศษ</a></h6>
               <p>กรรมการเลือกตั้ง มอบนโยบายในการปฏิบัติงาน แนวทางแก้ไขปัญหาในการปฏิบัติงานให้แก่พนักงานในการอบรมหลักสูตร</p>
               <p class="mt-3 text-sm">08 มิถุนายน 2565 / By <span class="text-sm"><a href="#">PR ECT</a></span></p>
             </div>
@@ -41,19 +79,23 @@
         </div>
         <div class="grid lg-30 md-40 sm-100 xs-100">
           <div class="pos-relative">
-            <img src="public/assets/app/images/bg/04.jpg" class="img bradius-round" style="height:252px;" />
+            <a href="#">
+              <img src="public/assets/app/images/bg/04.jpg" class="img bradius-round" style="height:252px;" />
+            </a>
             <div class="pos-absolute text-wrapper ss-text-wrapper sm color-white">
               <a href="#" class="d-inline-block bg-p bradius-round color-white pt-1 pr-3 pb-1 pl-3">เรื่องเด่น กกต.</a>
-              <h6 class="mt-3 fw-500 ">การประชุมพัฒนางานและยกระดับมาตรฐาน</h6>
+              <h6 class="mt-3 fw-500 "><a href="#">การประชุมพัฒนางานและยกระดับมาตรฐาน</a></h6>
               <p class="text-sm">08 มิถุนายน 2565 / By <span class="text-sm"><a href="#">PR ECT</a></span></p>
             </div>
           </div>
           <div class="grid lg-30 md-40 sm-100 xs-100">
             <div class="pos-relative pt-5">
-              <img src="public/assets/app/images/bg/05.jpg" class="img bradius-round" style="height:252px;" />
+              <a href="#">
+                <img src="public/assets/app/images/bg/05.jpg" class="img bradius-round" style="height:252px;" />
+              </a>
               <div class="pos-absolute text-wrapper ss-text-wrapper sm color-white">
                 <a href="#" class="d-inline-block bg-p bradius-round color-white pt-1 pr-3 pb-1 pl-3">เรื่องเด่น กกต.</a>
-                <h6 class="mt-3 fw-500">คณะกรรมการการเลือกตั้งครบรอบ 24 ปี</h6>
+                <h6 class="mt-3 fw-500"><a>คณะกรรมการการเลือกตั้งครบรอบ 24 ปี</a></h6>
                 <p class="text-sm">08 มิถุนายน 2565 / By <span><a href="#">PR ECT</a></span></p>
               </div>
             </div>
@@ -89,21 +131,21 @@
           </div>
           <div class="grids">
             <div class="pos-relative mr-3" >
-              <img src="public/assets/app/images/bg/07.png" class="img bradius-round" />
+              <!-- <img src="public/assets/app/images/bg/07.png" class="img bradius-round" /> -->
               <div class="pos-absolute text-wrapper">
-                <h6 class="text-center color-white fw-600">แอปพลิเคชั่นฉลาดเลือก</h6>
+                <!-- <h6 class="text-center color-white fw-600">แอปพลิเคชั่นฉลาดเลือก</h6> -->
               </div>
             </div>
             <div class="pos-relative mr-3">
-              <img src="public/assets/app/images/bg/08.png" class="img bradius-round" />
+              <!-- <img src="public/assets/app/images/bg/08.png" class="img bradius-round" /> -->
               <div class="pos-absolute text-wrapper">
-                <h6 class="text-center color-white fw-600">แอปพลิเคชั่นฉลาดเลือก</h6>
+                <!-- <h6 class="text-center color-white fw-600">แอปพลิเคชั่นฉลาดเลือก</h6> -->
               </div>
             </div>
             <div class="pos-relative mr-3">
-              <img src="public/assets/app/images/bg/09.png" class="img bradius-round" />
+              <!-- <img src="public/assets/app/images/bg/09.png" class="img bradius-round" /> -->
               <div class="pos-absolute text-wrapper">
-                <h6 class="text-center color-white fw-600">แอปพลิเคชั่นฉลาดเลือก</h6>
+                <!-- <h6 class="text-center color-white fw-600">แอปพลิเคชั่นฉลาดเลือก</h6> -->
               </div>
             </div>
           </div>
@@ -112,45 +154,41 @@
     </div>
   </section>
 
-  <section class="section-04 section-padding">
+  <section class="section-04 section-padding" style="background-image: url(public/assets/app/images/bg/10.jpg);">
     <div class="container">
       <h3 class="fw-700">องค์ความรู้การเลือกตั้ง</h3>
       <div class="grids">
-        <div class="grid lg-50">
+        <div class="grid lg-50 md-100 sm-100">
           <div class="grids">
-            <div class="grid lg-50">
-              <div class="ss-text-wrapper">
-                <h5 class="color-s fw-500">เลือกตั้งกรุงเทพมหานครและ <br> เมืองพัทยา</h5>
+            <div class="grid lg-50 md-50 sm-50">
+              <div class="text-wrapper ss-text-wrapper bradius-round p-5">
+                <h5 class="color-s fw-500">เลือกตั้งกรุงเทพมหานครและเมืองพัทยา</h5>
                 <p class="mt-2 text-nm fw-400">
-                  วันอาทิตย์ที่ 22 พฤษภาคม 2565 เวลา 08.00 - 17.00 น. เลือกตั้งสมาชิกสภาท้องถิ่นและผู้
-                  บริหารท้องถิ่นของกรุงเทพมหานครและเมือง
+                  วันอาทิตย์ที่ 22 พฤษภาคม 2565 เวลา 08.00 - 17.00 น. เลือกตั้งสมาชิกสภาท้องถิ่นและผู้บริหารท้องถิ่นของ
                 </p>
               </div>
             </div>
-            <div class="grid lg-50">
-              <div class="ss-text-wrapper">
+            <div class="grid lg-50 md-50 sm-50">
+              <div class="text-wrapper ss-text-wrapper bradius-round p-5">
                 <h5 class="color-s fw-500">การบริจาคภาษีแก่พรรค <br> การเมือง</h5>
                 <p class="mt-2 text-nm fw-400">
-                  วันอาทิตย์ที่ 22 พฤษภาคม 2565 เวลา 08.00 - 17.00 น. เลือกตั้งสมาชิกสภาท้องถิ่นและผู้
-                  บริหารท้องถิ่นของกรุงเทพมหานครและเมือง
+                  ประชาชนผู้เสียภาษีที่เป็นบุคคลธรรมดาสามารถมีส่วนร่วมทางการเมืองได้ ด้วยผ่านการบริจาคภาษีเงิน
                 </p>
               </div>
             </div>
-            <div class="grid lg-50">
-              <div class="ss-text-wrapper">
+            <div class="grid lg-50 md-50 sm-50">
+              <div class="text-wrapper ss-text-wrapper bradius-round p-5">
                 <h5 class="color-s fw-500">เลือกตั้ง อบต.</h5>
                 <p class="mt-2 text-nm fw-400">
-                  วันอาทิตย์ที่ 22 พฤษภาคม 2565 เวลา 08.00 - 17.00 น. เลือกตั้งสมาชิกสภาท้องถิ่นและผู้
-                  บริหารท้องถิ่นของกรุงเทพมหานครและเมือง
+                  วันอาทิตย์ที่ 28 พฤศจิกายน 2564 เวลา 08.00 - 17.00 น. เลือกตั้งสมาชิกสภาองค์การบริหารส่วนตำบลและ
                 </p>
               </div>
             </div>
-            <div class="grid lg-50">
-              <div class="ss-text-wrapper">
-                <h5 class="color-s fw-500">การแจ้งเหตุไม่อาจไปใช้สิทธิ <br> เลือกตั้งทางสื่ออิเล็กทรอนิกส์</h5>
+            <div class="grid lg-50 md-50 sm-50">
+              <div class="text-wrapper text-wrapper bradius-round p-5">
+                <h5 class="color-s fw-500">การแจ้งเหตุไม่อาจไปใช้สิทธิเลือกตั้งทางสื่ออิเล็กทรอนิกส์</h5>
                 <p class="mt-2 text-nm fw-400">
-                  วันอาทิตย์ที่ 22 พฤษภาคม 2565 เวลา 08.00 - 17.00 น. เลือกตั้งสมาชิกสภาท้องถิ่นและผู้
-                  บริหารท้องถิ่นของกรุงเทพมหานครและเมือง
+                  เพื่อเป็นการเพิ่มช่องทางอำนวยความสะดวกแก่ประชาชนผู้มีสิทธิเลือกตั้งที่มีความจำเป็นไม่อาจไปใช้สิทธิเลือกตั้ง
                 </p>
               </div>
             </div>
@@ -165,19 +203,19 @@
       <h3 class="text-center fw-700">บทความพิเศษ</h3>
       <div class="grids">
         <div class="grid lg-1-3 md-50 sm-50 xs-100">
-          <img src="public/assets/app/images/bg/demo06.jpg" class="img bradius-round" />
+          <img src="public/assets/app/images/bg/11.jpg" class="img bradius-round" />
           <h5 class="mt-5">วารสารสำงานกรรมการเลือกตั้ง:พรรค การเมืองกับการเปลี่ยนแปลงทางการเมืองและการเลือกตั้ง</h5>
           <p class="mt-3">08 มิถุนายน 2565 / By <span><a href="#">Admin</a></span></p>
           <p>หยวนออร์แกนิค โบรกเกอร์บ๊อบ รีทัชแคนยอน แบล็คซะบรรพชน แชมเปี้ยน จัมโบ้มาร์ตออดิทอเรียม ลีก เชฟคองเกรสเยลลี่ เพรสแคมป์สี่แยกยูโร กุนซือแบรนด์ ตุ๊กเวเฟอร์โชห่วย แหวว ต่อรองช็อปปิ้งเก๊ะแบล็คลิมิต ฮันนีมูนเปียโนคอรัปชันห่วย แตงกวาช็อปเปอร์แมนชั่น เป็นไงแทงกั๊กลีก</p>
         </div>
         <div class="grid lg-1-3 md-50 sm-50 xs-100">
-          <img src="public/assets/app/images/bg/demo06.jpg" class="img bradius-round" />
+          <img src="public/assets/app/images/bg/12.jpg" class="img bradius-round" />
           <h5 class="mt-5">วารสารสำงานกรรมการเลือกตั้ง:พรรค การเมืองกับการเปลี่ยนแปลงทางการเมืองและการเลือกตั้ง</h5>
           <p class="mt-3">08 มิถุนายน 2565 / By <span><a href="#">Admin</a></span></p>
           <p>หยวนออร์แกนิค โบรกเกอร์บ๊อบ รีทัชแคนยอน แบล็คซะบรรพชน แชมเปี้ยน จัมโบ้มาร์ตออดิทอเรียม ลีก เชฟคองเกรสเยลลี่ เพรสแคมป์สี่แยกยูโร กุนซือแบรนด์ ตุ๊กเวเฟอร์โชห่วย แหวว ต่อรองช็อปปิ้งเก๊ะแบล็คลิมิต ฮันนีมูนเปียโนคอรัปชันห่วย แตงกวาช็อปเปอร์แมนชั่น เป็นไงแทงกั๊กลีก</p>
         </div>
         <div class="grid lg-1-3 md-50 sm-50 xs-100">
-          <img src="public/assets/app/images/bg/demo06.jpg" class="img bradius-round" />
+          <img src="public/assets/app/images/bg/13.jpg" class="img bradius-round" />
           <h5 class="mt-5">วารสารสำงานกรรมการเลือกตั้ง:พรรค การเมืองกับการเปลี่ยนแปลงทางการเมืองและการเลือกตั้ง</h5>
           <p class="mt-3">08 มิถุนายน 2565 / By <span><a href="#">Admin</a></span></p>
           <p>หยวนออร์แกนิค โบรกเกอร์บ๊อบ รีทัชแคนยอน แบล็คซะบรรพชน แชมเปี้ยน จัมโบ้มาร์ตออดิทอเรียม ลีก เชฟคองเกรสเยลลี่ เพรสแคมป์สี่แยกยูโร กุนซือแบรนด์ ตุ๊กเวเฟอร์โชห่วย แหวว ต่อรองช็อปปิ้งเก๊ะแบล็คลิมิต ฮันนีมูนเปียโนคอรัปชันห่วย แตงกวาช็อปเปอร์แมนชั่น เป็นไงแทงกั๊กลีก</p>
@@ -191,12 +229,12 @@
     </div>
   </section>
 
-  <section class="section-06 section-padding" style="background-color:#0ab3de">
+  <section class="section-06 section-padding" style="background-image: url(public/assets/app/images/bg/14.jpg);">
     <div class="container">
       <div class="grids">
         <div class="grid lg-25 md-50 sm-50 xs-100">
           <div class="pos-relative">
-            <img src="public/assets/app/images/bg/demo07.jpg" class="img bradius-round" />
+            <img src="public/assets/app/images/bg/15.png" class="img bradius-round" />
             <div class="pos-absolute text-wrapper">
               <h4>คำวินิจฉัย</h4>
               <h6>คณะกรรมการการเลือกตั้ง</h6>
@@ -206,7 +244,7 @@
         </div>
         <div class="grid lg-25 md-50 sm-50 xs-100">
           <div class="pos-relative">
-            <img src="public/assets/app/images/bg/demo07.jpg" class="img bradius-round" />
+            <img src="public/assets/app/images/bg/16.png" class="img bradius-round" />
             <div class="pos-absolute text-wrapper">
               <h4>รวมถาม-ตอบ</h4>
               <h6>ข้อซักถามพรรคการเมือง</h6>
@@ -216,7 +254,7 @@
         </div>
         <div class="grid lg-25 md-50 sm-50 xs-100">
           <div class="pos-relative">
-            <img src="public/assets/app/images/bg/demo07.jpg" class="img bradius-round" />
+            <img src="public/assets/app/images/bg/17.png" class="img bradius-round" />
             <div class="pos-absolute text-wrapper">
               <h4>สายด่วน<span class="">1444</span></h4>
               <h6>คณะกรรมการการเลือกตั้ง</h6>
@@ -226,7 +264,7 @@
         </div>
         <div class="grid lg-25 md-50 sm-50 xs-100">
           <div class="pos-relative">
-            <img src="public/assets/app/images/bg/demo07.jpg" class="img bradius-round" />
+            <img src="public/assets/app/images/bg/18.png" class="img bradius-round" />
             <div class="pos-absolute text-wrapper">
               <h4>Poll Vote</h4>
               <h6>คณะกรรมการการเลือกตั้ง</h6>
